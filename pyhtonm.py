5e18f88893e889a643ccc8420a6e8657c49ce4cb
Alejandro Vélez <141779507+MelonConYogurt@users.noreply.github.com>
2024-07-24 21:57:29 -0500

    Update commit

--- pyhtonm.py
+++
@@ -1,9 +0,0 @@
-diccionario = {'nombre': 'Alejandro', 'edad': 30}
-claves = diccionario.keys()  # Obtiene el valor 'Alejandro'
-valores = diccionario.values()
-pares = diccionario.items()
-print(pares)
-print(diccionario)
-print(claves)
-print(valores)
-
